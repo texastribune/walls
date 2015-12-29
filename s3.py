c557a8fdc6bf5cc3b5b8ee816e6c056119f7d579
Daniel Craigmile <dcraigmile@texastribune.org>
2015-12-29 17:57:51 -0600

    add function to push to s3 with proper caching

---
+++ s3.py
@@ -0,0 +1,33 @@
+from datetime import datetime, timedelta
+
+from boto.s3.connection import S3Connection, OrdinaryCallingFormat
+from boto.s3.key import Key
+
+# This value should be set to the frequency that this job is run. Right now I
+# believe it will be called every 12 hours. If that changes this should too:
+HOURS_TO_EXPIRE = 12
+
+
+def push_to_s3(filename=None, contents=None):
+    """
+    Save a file to the the membership bucket with name and contents
+    specified in the call.
+
+    This sets the contents to be publicly readable, cacheable by
+    intermediaries with an expiration date a specified number
+    of hours from when this job is run. (See above.)
+    """
+
+    conn = S3Connection(calling_format=OrdinaryCallingFormat())
+    bucket = conn.get_bucket('membership.texastribune.org')
+    k = Key(bucket)
+    k.key = filename
+    expires = datetime.utcnow() + timedelta(hours=HOURS_TO_EXPIRE)
+    expires = expires.strftime("%a, %d %b %Y %H:%M:%S GMT")
+    k.set_contents_from_string(contents,
+            policy='public-read',
+            headers={
+                'Cache-Control': 'public',
+                'Expires': '{}'.format(expires)
+                })
+    k.set_acl('public-read')
