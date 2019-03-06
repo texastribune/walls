d9dba17d79465b7119fa81aad128f74ae70140ba
Daniel Craigmile <dcraigmile@texastribune.org>
2019-03-05 19:13:55 -0600

    update S3 upload for Python 3

--- s3.py
+++ s3.py
@@ -1,9 +1,7 @@
 from datetime import datetime, timedelta
 import gzip
-import StringIO
-
-from boto.s3.connection import S3Connection, OrdinaryCallingFormat
-from boto.s3.key import Key
+from io import BytesIO
+import boto3
 
 from config import HOURS_TO_EXPIRE, BUCKET
 
@@ -19,23 +17,25 @@ def push_to_s3(filename=None, contents=None):
     intermediaries with an expiration date a specified number
     of hours from when this job is run. (See above.)
     """
-
-    out = StringIO.StringIO()
+    contents = bytes(contents, "utf-8")
+    out = BytesIO()
     with gzip.GzipFile(fileobj=out, mode="w") as f:
         f.write(contents)
+    out.seek(0)
 
-    conn = S3Connection(calling_format=OrdinaryCallingFormat())
-    bucket = conn.get_bucket(BUCKET)
-    k = Key(bucket)
-    k.key = filename
     expires = datetime.utcnow() + timedelta(hours=HOURS_TO_EXPIRE)
     expires = expires.strftime("%a, %d %b %Y %H:%M:%S GMT")
-    k.set_contents_from_string(out.getvalue(),
-            policy='public-read',
-            headers={
-                'Cache-Control': 'public',
-                'Content-Type': 'application/json',
-                'Content-Encoding': 'gzip',
-                'Expires': '{}'.format(expires)
-                })
-    k.set_acl('public-read')
+
+    s3 = boto3.client("s3")
+    s3.upload_fileobj(
+        out,
+        BUCKET,
+        filename,
+        ExtraArgs={
+            "ContentType": "application/json",
+            "ACL": "public-read",
+            "CacheControl": "public",
+            "Expires": expires,
+            "ContentEncoding": "gzip",
+        },
+    )
